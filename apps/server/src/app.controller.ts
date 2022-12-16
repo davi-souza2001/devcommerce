ff33bb4415b3d0d5055a17a34ae168325f35a0c3
davi-souza2001 <davisouza2001dv@gmail.com>
2022-12-16 14:51:27 -0300

    up

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { AppService } from './app.service'
+import { PrismaService } from './prisma.service'
+import { randomUUID } from 'node:crypto'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: PrismaService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('users')
+  list() {
+    return this.appService.user.findMany()
+  }
+
+  @Post('users')
+  async create(@Body() body: any) {
+    const { email, name, photo } = body
+
+    await this.appService.user.create({
+      data: {
+        id: randomUUID(),
+        email,
+        name,
+        photo
+      }
+    })
   }
 }
