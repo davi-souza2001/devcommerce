fc6e804960ef529392fd0b6759f0276eb3423841
Davi Souza <davisouza2001dv@gmail.com>
2023-01-13 21:13:14 -0300

    up

--- apps/web/src/components/TopBar.tsx
+++ apps/web/src/components/TopBar.tsx
@@ -1,17 +1,18 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Router from 'next/router';
 
-import Logo from '../../public/logo.svg'
+import Logo from '../../public/logo.svg';
 
 export function TopBar() {
 	return (
 		<div className="h-20 w-full flex items-center justify-around bg-black text-slate-200 text-xl font-regular uppercase transition-all">
-			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100">Home</span>
+			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100" onClick={() => Router.push('/')}>Home</span>
 			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100">Categories</span>
 			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100">Search</span>
 			<Image src={Logo} alt='Logo' height={100} width={100} />
 			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100">Cart</span>
 			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100">Withlist</span>
-			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100">Account</span>
+			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100" onClick={() => Router.push('/account')}>Account</span>
 		</div>
 	)
 }
--- apps/web/src/pages/account.tsx
+++ apps/web/src/pages/account.tsx
@@ -1,9 +1,37 @@
+import Image from 'next/image'
+
 import { TopBar } from "../components/TopBar";
+import Perfil from '../../public/Perfil.jpg';
+import { InputField } from '../components/InputField';
+import { ButtonField } from '../components/ButtonField';
 
 export default function Account() {
 	return (
 		<div className="h-screen w-screen">
 			<TopBar />
+			<div className='h-5/6 w-full flex items-start justify-around'>
+				<div className='h-[320px] w-80 bg-slate-200 rounded-lg mt-10'>
+					<div className='h-2/4 w-full flex items-center justify-center'>
+						<Image src={Perfil} alt="Image Profile" height={150} width={150} className="rounded-full" />
+					</div>
+					<div className='h-1/4 w-full flex items-center justify-center text-2xl font-semibold'>
+						<p>Davi Souza</p>
+					</div>
+					<div className='h-1/4 w-full flex items-center justify-center text-base font-medium border border-t-slate-400 rounded-lg'>
+						<p>Membro desde: janeiro, 2021 </p>
+					</div>
+				</div>
+				<div className='min-h-[320px] w-[800px] bg-slate-200 rounded-lg  mt-10'>
+					<div className='h-16 w-full flex items-center justify-start text-xl font-semibold p-4'>
+						<p>Seus dados:</p>
+					</div>
+					<div className='w-full flex flex-col items-center justify-start text-xl font-semibold p-4'>
+						<InputField placeHolder='Nome' type='string' className='w-full mb-5'/>
+						<InputField placeHolder='E-mail' type='string' className='w-full mb-5'/>
+						<ButtonField text='Alterar' className='w-32'/>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
