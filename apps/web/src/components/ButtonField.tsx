65b72112ae1886c79d0ec5df4ffc20436f51d524
Davi Souza <davisouza2001dv@gmail.com>
2023-01-14 21:23:28 -0300

    up

--- apps/web/src/components/ButtonField.tsx
+++ apps/web/src/components/ButtonField.tsx
@@ -1,12 +1,17 @@
-interface ButtonField{
+interface ButtonField {
 	text: string
 	color?: string
 	className?: string
+	type?: "button" | "submit" | "reset"
+	onClick?: () => void
 }
 
 export function ButtonField(props: ButtonField) {
 	return (
-		<button className={`h-12 flex items-center justify-center  rounded-lg text-xl font-semibold text-slate-200 transition-all ${props.color ? `bg-${props.color} hover:bg` : "bg-green-500 hover:bg-green-600"} ${props.className}`}>
+		<button className={`h-12 flex items-center justify-center  rounded-lg text-xl font-semibold text-slate-200 transition-all ${props.color ? `bg-${props.color} hover:bg` : "bg-green-500 hover:bg-green-600"} ${props.className}`}
+			type={props.type ?? 'submit'}
+			onClick={props.onClick}
+		>
 			{props.text}
 		</button>
 	)
--- apps/web/src/components/InputField.tsx
+++ apps/web/src/components/InputField.tsx
@@ -1,15 +1,22 @@
+import { ChangeEventHandler, useEffect, useState } from "react"
+
 interface InputFieldProps {
 	type: string
 	placeHolder: string
 	className?: string
+	value: string
+	onChange: ChangeEventHandler<HTMLInputElement>
 }
 
 export function InputField(props: InputFieldProps) {
+
 	return (
 		<input
 			className={`h-16 rounded-lg outline-none text-gray-500 font-bold ${props.className}`}
 			type={props.type}
 			placeholder={props.placeHolder}
+			onChange={props.onChange}
+			value={props.value}
 		/>
 	)
 }
