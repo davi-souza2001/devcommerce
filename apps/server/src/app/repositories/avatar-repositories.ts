c7b363cbc8bd48ec19ace0a9aa87946a2833ce13
Davi Souza <davisouza2001dv@gmail.com>
2023-01-25 16:28:09 -0300

    up

--- apps/server/src/app/repositories/avatar-repositories.ts
+++
@@ -1,5 +0,0 @@
-import { Avatar } from "../entities/avatar";
-
-export abstract class AvatarRepository {
-	abstract create(user: Avatar): Promise<void>
-}
--- apps/server/src/app/repositories/user-repositories.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "../entities/user";
-
-export abstract class UserRepository {
-	abstract create(user: User): Promise<void>
-	abstract login(email: string): Promise<User | any>
-}
