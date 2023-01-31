d9cadf871d55f54cad1ff20516b45ef7876b7081
Davi Souza <davisouza2001dv@gmail.com>
2023-01-31 15:12:34 -0300

    up

--- apps/web/src/pages/account.tsx
+++ apps/web/src/pages/account.tsx
@@ -1,24 +1,59 @@
-import Image from 'next/image'
 import Router from 'next/router'
 import { useEffect, useState } from 'react'
+import { AvatarProps, BigHead } from '@bigheads/core'
 
 import { TopBar } from "../components/TopBar"
-import Perfil from '../../public/Perfil.jpg'
 import { InputField } from '../components/InputField'
 import { ButtonField } from '../components/ButtonField'
 import { BackgroundField } from '../components/BackgroundField'
 import UseAuth from '../service/hook/useAuth'
+import UseFetch from '../service/hook/useFetch'
 
 export default function Account() {
 	const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
+	const [avatar, setAvatar] = useState<AvatarProps>({ body: "chest", skinTone: "light" })
+	const [avatarExists, setAvatarExists] = useState(false)
 
 	const { logoutInFirebase, user } = UseAuth()
+
+	function getAvatarExists() {
+		UseFetch('http://localhost:3333/avatar/get', 'POST', { id: user.id })
+			.then((avatarReceived: AvatarProps) => {
+				if (avatarReceived !== null) {
+					setAvatar({
+						accessory: avatarReceived.accessory,
+						body: avatarReceived.body,
+						circleColor: avatarReceived.circleColor,
+						clothing: avatarReceived.clothing,
+						clothingColor: avatarReceived.clothingColor ?? "green",
+						eyebrows: avatarReceived.eyebrows,
+						eyes: avatarReceived.eyes,
+						faceMask: avatarReceived.faceMask,
+						facialHair: avatarReceived.facialHair,
+						graphic: avatarReceived.graphic,
+						hair: avatarReceived.hair,
+						hairColor: avatarReceived.hairColor,
+						hat: avatarReceived.hat,
+						hatColor: avatarReceived.hatColor,
+						lipColor: avatarReceived.lipColor,
+						mouth: avatarReceived.mouth,
+						skinTone: avatarReceived.skinTone
+					})
+
+					setAvatarExists(true)
+				}
+			})
+	}
 
 	useEffect(() => {
 		if (user) {
 			setEmail(user.email)
 			setName(user.name)
+		}
+
+		if (user.id) {
+			getAvatarExists()
 		}
 	}, [user])
 
@@ -29,7 +64,33 @@ export default function Account() {
 				<div className='h-5/6 w-10/12 bg-slate-300 rounded-lg mt-10'>
 					<div className='h-1/3 w-full flex items-center justify-between'>
 						<div className='h-full w-96 flex items-center justify-center'>
-							<Image alt='Image profile' src={Perfil} height={150} width={150} className='rounded-full' />
+							<div className='h-60 w-48'>
+								{avatarExists ? (
+									<BigHead
+										accessory={avatar.accessory ?? 'none'}
+										body={avatar.body ?? 'chest'}
+										circleColor='blue'
+										clothing={avatar.clothing ?? 'naked'}
+										clothingColor={avatar.clothingColor ?? 'black'}
+										eyebrows={avatar.eyebrows ?? 'serious'}
+										eyes={avatar.eyes ?? 'normal'}
+										faceMask={avatar.faceMask ?? false}
+										faceMaskColor="white"
+										facialHair={avatar.facialHair ?? 'none'}
+										graphic={avatar.graphic ?? 'none'}
+										hair={avatar.hair ?? 'none'}
+										hairColor={avatar.hairColor ?? 'black'}
+										hat={avatar.hat ?? 'none'}
+										hatColor={avatar.hatColor ?? 'black'}
+										lashes={false}
+										lipColor={avatar.lipColor ?? 'turqoise'}
+										mouth={avatar.mouth ?? 'serious'}
+										skinTone={avatar.skinTone ?? 'light'}
+									/>
+								) : (
+									<p>Criar</p>
+								)}
+							</div>
 							<p className='ml-5 text-lg font-semibold'>Davi Souza</p>
 							<p className='ml-5 text-xl font-semibold cursor-pointer'
 								onClick={() => Router.push('/avatar')}
@@ -56,6 +117,6 @@ export default function Account() {
 					</div>
 				</div>
 			</div>
-		</BackgroundField>
+		</BackgroundField >
 	)
 }
