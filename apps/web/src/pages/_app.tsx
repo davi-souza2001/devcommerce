e4736f797995af380dbd39cafe4d15e28c8b9504
Davi Souza <davisouza2001dv@gmail.com>
2023-01-31 14:57:38 -0300

    up

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head'
 import { AuthProvider } from '../service/context/AuthContext'
 import '../styles/globals.css'
 
 function App({ Component, pageProps }: any) {
 	return (
 		<AuthProvider>
+			<Head>
+				<link rel="icon" type="svg" sizes="32x32" href="/logo.svg" />
+				<title>JoGeeker</title>
+			</Head>
 			<Component {...pageProps} />
 		</AuthProvider>
 	)
