b330521b94b8634528dc712025e724a77458b199
Davi Souza <davisouza2001dv@gmail.com>
2023-02-07 17:43:18 -0300

    up

--- apps/web/src/components/TopBar.tsx
+++ apps/web/src/components/TopBar.tsx
@@ -8,7 +8,7 @@ export function TopBar() {
 		<div className="h-20 w-full flex items-center justify-around bg-black text-slate-200 text-xl font-regular uppercase transition-all">
 			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100" onClick={() => Router.push('/')}>Home</span>
 			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100">Categories</span>
-			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100">Search</span>
+			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100" onClick={() => Router.push('/search')}>Search</span>
 			<Image src={Logo} alt='Logo' height={100} width={100} />
 			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100">Cart</span>
 			<span className="cursor-pointer opacity-80 transition-all hover:opacity-100">Withlist</span>
---
+++ apps/web/src/pages/search.tsx
@@ -0,0 +1,11 @@
+import { BackgroundField } from "../components/BackgroundField";
+import { TopBar } from "../components/TopBar";
+
+export default function Search(){
+	return (
+		<BackgroundField>
+			<TopBar/>
+			<h1>Search</h1>
+		</BackgroundField>
+	)
+}
